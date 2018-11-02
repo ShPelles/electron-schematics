2a2b1c1cb10c2e8efb78b04d9b16711b59bd14ad
shalom <shalom@haravdotnet.co.il>
2018-11-02 10:33:20 +0200

    0.6.0

--- packages/schematics/src/utility/latest-versions.ts
+++ packages/schematics/src/utility/latest-versions.ts
@@ -1,6 +1,6 @@
 export const latestVersions = {
   eletron: '^3.0.0',
-  ['@electron-schematics/build-electron']: '^0.5.1',
+  ['@electron-schematics/build-electron']: '^0.6.0',
   ['ts-loader']: '^5.2.1',
   ['copy-webpack-plugin']: '^4.5.2',
 };
