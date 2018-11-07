7374677ca8159117d3505a6983bf4154dfed07b1
ShPelles <43875468+ShPelles@users.noreply.github.com>
2018-11-07 09:05:51 +0200

    run azure pipelines in multiple OSs (#9)
    * pipelines templates: run under multiple OSs &  multiple angular versions
    
    * fix e2e test

--- e2e.sh
+++ e2e.sh
@@ -14,14 +14,17 @@ npm pack
 
 npm i @angular/cli -g
 
+# goto e2e directory
+cd ../../e2e$2
+# echo $PWD
+
 # install packages & generate electron project
-cd ../../e2e
 npm i ../packages/schematics/electron-schematics-schematics-0.0.1.tgz
 ng g @electron-schematics/schematics:electron
 npm i ../packages/build-electron/electron-schematics-build-electron-0.0.1.tgz
 
 # run the app with auto exit
-mv --force 'main.ts' 'projects/electron/main.ts'
+cp 'main.ts' 'projects/electron/main.ts'
 ng serve electron
 
 # check if log file exists
@@ -32,5 +35,10 @@ ng serve electron
 # try to build the app
 rm -r ./dist
 ng build electron
+
+# check if electron app exists
+if [[ ! -f $1 ]] ; then
+    exit 1
+fi
 
 # read -p "Press enter to continue"
