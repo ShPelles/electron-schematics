4dda56b2c0166345faba150714f73c8b8ee60414
ShPelles <43875468+ShPelles@users.noreply.github.com>
2018-11-14 18:04:49 +0200

    Fix e2e test (#13)
    * ignore serve test on Linux

--- e2e.sh
+++ e2e.sh
@@ -15,7 +15,7 @@ npm pack
 npm i @angular/cli -g
 
 # goto e2e directory
-cd ../../e2e$2
+cd ../../e2e$3
 echo 'tested version is in ' $PWD
 
 # install packages & generate electron project
@@ -23,15 +23,19 @@ npm i ../packages/schematics/electron-schematics-schematics-0.0.1.tgz
 ng g @electron-schematics/schematics:electron
 npm i ../packages/build-electron/electron-schematics-build-electron-0.0.1.tgz
 
-# run the app with auto exit
-cp 'main.ts' 'projects/electron/main.ts'
-LOG=$(ng serve electron)
-echo $LOG
 
-# check the log
-# if [[ ! $LOG == *"Angualr say: on init"* ]] ; then
-#     exit 1
-# fi
+
+# run the app with auto exit & Check the log
+cp 'main.ts' 'projects/electron/main.ts'    
+LOG=$(ng serve electron);
+
+if [[ ! $2 == "ubuntu"* ]] ; then
+    if [[ ! $LOG == *"Angualr say: on init"* ]] ; then
+        exit 1
+    fi
+
+fi
+
 
 # try to build the app
 rm -r ./dist
