ad4062a037b7d4c7dd9274a7aabc61630b8d5e05
Eran Shmil <eranshmil@users.noreply.github.com>
2018-12-19 10:38:40 +0200

    build: automatic builder version (#23)
    * build: automatic builder version
    
    * build(e2e): change to dynamic version

--- packages/schematics/src/utility/latest-versions.ts
+++ packages/schematics/src/utility/latest-versions.ts
@@ -1,6 +1,8 @@
+const packageJson = require('../../package.json');
+
 export const latestVersions = {
-  eletron: '^3.0.0',
-  ['@electron-schematics/build-electron']: '^0.7.0',
+  electron: '^3.0.0',
+  ['@electron-schematics/build-electron']: `^${packageJson.version}`,
   ['ts-loader']: '^5.2.1',
   ['copy-webpack-plugin']: '^4.5.2',
 };
