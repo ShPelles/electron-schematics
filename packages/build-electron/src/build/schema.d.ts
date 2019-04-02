57d8d35e55f0fe7916a47f1cb9bcfe87299795d4
ShPelles <shalom.pelles@gmail.com>
2019-04-02 18:27:14 +0300

    fix add CustomWebpackSchema to schemas

--- packages/build-electron/src/build/schema.d.ts
+++ packages/build-electron/src/build/schema.d.ts
@@ -1,6 +1,7 @@
 import { BrowserBuilderSchema } from "@angular-devkit/build-angular";
 import { WebpackBuilderSchema } from "@angular-devkit/build-webpack/src/webpack/schema";
+import { CustomWebpackSchema } from "@angular-builders/custom-webpack/typings/custom-webpack-schema";
 
-export interface ElectronBuilderSchema extends BrowserBuilderSchema, WebpackBuilderSchema  {
+export interface ElectronBuilderSchema extends BrowserBuilderSchema, WebpackBuilderSchema, CustomWebpackSchema  {
     relatedApp: string;
 }
--- packages/build-electron/src/start/schema.d.ts
+++ packages/build-electron/src/start/schema.d.ts
@@ -1,6 +1,7 @@
 import { DevServerBuilderOptions } from '@angular-devkit/build-angular';
 import { WebpackBuilderSchema } from '@angular-devkit/build-webpack/src/webpack/schema';
+import { CustomWebpackSchema } from '@angular-builders/custom-webpack/typings/custom-webpack-schema';
 
-export interface ElectronStartBuilderSchema extends DevServerBuilderOptions, WebpackBuilderSchema {
+export interface ElectronStartBuilderSchema extends DevServerBuilderOptions, WebpackBuilderSchema, CustomWebpackSchema {
 
 }
