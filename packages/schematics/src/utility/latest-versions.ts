ea299f4ae3738bd59421236190642465af5b814f
ShPelles <shalom.pelles@gmail.com>
2019-04-16 13:57:46 +0300

    update-dependencies

--- packages/schematics/src/utility/latest-versions.ts
+++ packages/schematics/src/utility/latest-versions.ts
@@ -1,7 +1,7 @@
 const packageJson = require('../../package.json');
 
 export const latestVersions = {
-  electron: '^3.0.0',
+  electron: '^4.0.0',
   ['@electron-schematics/build-electron']: `^${packageJson.version}`,
   ['ts-loader']: '^5.2.1',
   ['copy-webpack-plugin']: '^4.5.2',
