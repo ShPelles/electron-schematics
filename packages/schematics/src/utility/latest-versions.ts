a556f33657c455523bbe8c863ba3b1cfa878a3a8
ShPelles <shalom.pelles@gmail.com>
2019-05-07 17:15:28 +0300

    move electron & electron-builder to 'peerDependencies' (fix #28)

--- packages/schematics/src/utility/latest-versions.ts
+++ packages/schematics/src/utility/latest-versions.ts
@@ -2,6 +2,7 @@ const packageJson = require('../../package.json');
 
 export const latestVersions = {
   electron: '^3.0.0',
+  ['electron-builder']: '^20.28.4',
   ['@electron-schematics/build-electron']: `^${packageJson.version}`,
   ['ts-loader']: '^5.2.1',
   ['copy-webpack-plugin']: '^4.5.2',
